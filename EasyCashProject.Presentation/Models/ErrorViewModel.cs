1caf4fafc02c1a7c090dc07abe00281a344f646f
Samet DOGAN <sametdogan23@protonmail.ch>
2023-09-03 13:18:02 +0300

    Add project files.

---
+++ EasyCashProject.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyCashProject.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
