624526c271a5c9f3b0373595c6044aa4e333984c
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-02 16:11:51 +0300

    layout edits

---
+++ EasyCashProject.Presentation/Controllers/MyAccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashProject.Presentation.Controllers;
+
+public class MyAccountsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
